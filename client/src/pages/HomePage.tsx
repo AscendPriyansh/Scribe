154abd0012b1400bc5748570f358989355353e8b
Priyansh <electroniorje@gmail.com>
2026-07-31 15:10:53 +0530

    Dashboard Section

--- client/src/pages/HomePage.tsx
+++ client/src/pages/HomePage.tsx
@@ -1,11 +1,19 @@
+import { ChartAreaGradient } from "@/components/ui/ChartAreaGradient";
+import { ChartPieLabel } from "@/components/ui/ChartPieLabel";
+import { ChartRadarDefault } from "@/components/ui/ChartRadarDefault";
+import { ChartRadialText } from "@/components/ui/ChartRadialText";
+
 function HomePage() {
   return (
     <div className="flex flex-1 flex-col gap-4 p-4">
       <h1 className="font-bold text-xl">Dashboard</h1>
-      <div className="min-h-screen flex-1 rounded-xl bg-muted/50 md:min-h-min flex flex-col justify-center items-center gap-3">
-        <div className="flex flex-col justify-center items-center">
-        
+      <div className="min-h-screen flex-1 rounded-xl bg-muted/50 p-4 flex flex-col gap-4">
+        <ChartAreaGradient title={"Visitors"} description={"Last 6 months visitors graph."} />
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-4 w-full">
+          <ChartPieLabel />
+          <ChartRadialText />
         </div>
+        <ChartRadarDefault />
       </div>
     </div>
   )
